242d65b64b5c01f3b6663ed56df950ece04f8caf
Alexander Tih <dgioto@gmail.com>
2021-03-20 23:03:36 +0200

    reset to start

--- app/src/main/java/com/example/bitandpizzas/MainActivity.java
+++ app/src/main/java/com/example/bitandpizzas/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.bitandpizzas;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //заменяем панель инструментов панелью приложения
+
     }
 }
