88957aef787facae4ebae6138b20669217ea0bfa
Alexander Tih <dgioto@gmail.com>
2021-03-22 13:15:51 +0200

    Обработка выбора элементов действий

--- app/src/main/java/com/example/bitandpizzas/MainActivity.java
+++ app/src/main/java/com/example/bitandpizzas/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.bitandpizzas;
 
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_mein, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    //Обработка выбора элементов действий
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
