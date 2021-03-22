007fd3b132143ead0ebd82a2b7c55f4fa7cdeec7
Alexander Tih <dgioto@gmail.com>
2021-03-22 23:22:43 +0200

    передача информации с панели приложения

--- app/src/main/java/com/example/bitandpizzas/MainActivity.java
+++ app/src/main/java/com/example/bitandpizzas/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.bitandpizzas;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_mein, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        //Получить ссылку на провайдера действия передачи информации и присвоить ее приватной переменной
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to join for pizza?");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    //Создаем метод, который создает интент и передает его провайдеру действия передачи информации
+    // при помощи его метода setShareIntent()
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
 
     //Обработка выбора элементов действий
