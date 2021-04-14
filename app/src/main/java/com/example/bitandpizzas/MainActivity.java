7c461b238dc2dcf2ceecdf6a34cd6939d4eadbc1
Alexander Tih <dgioto@gmail.com>
2021-04-14 23:14:25 +0300

    add ViewPager

--- app/src/main/java/com/example/bitandpizzas/MainActivity.java
+++ app/src/main/java/com/example/bitandpizzas/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.bitandpizzas;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
@@ -23,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         //заменяем панель инструментов панелью приложения
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Связывание SectionsPagerAdapter c ViewPager
+        //используются фрагменты из библиотеки поддержки, поэтому адаптеру необходимо передать
+        //ссылку на диспетчерфрагментов поддержки
+        SectionsPagerAdapter pagerAdapter =
+                new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        //Написанный ранее код FragmentPagerAdapter присоединяется к ViewPager
+        pager.setAdapter(pagerAdapter);
     }
 
     //Добавляем меню на панель приложения
@@ -56,6 +69,36 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //адаптер страничного компонента фрагментов
+    private class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        //необходим для указания какой фрагмент далжен выводиться на каждой странице
+        @Override
+        public Fragment getItem(int i) {
+            switch (i){
+                case  0:
+                    return  new TopFragment();
+                case  1:
+                    return new PizzaFragment();
+                case 2:
+                    return new PastaFragment();
+                case 3:
+                    return  new StorFragment();
+            }
+            return null;
+        }
+
+        //необходим для определения количества страниц в ViewPager
+        @Override
+        public int getCount() {
+            return 4;
         }
     }
 }
