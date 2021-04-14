ba805dbfaad3dbf5b26f071ef0dbabe10d054de2
Alexander Tih <dgioto@gmail.com>
2021-04-14 11:28:22 +0300

    Создание StoresFragment

--- app/src/main/java/com/example/bitandpizzas/StorFragment.java
+++ app/src/main/java/com/example/bitandpizzas/StorFragment.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-public class PastaFragment extends ListFragment {
+public class StorFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,7 +17,7 @@ public class PastaFragment extends ListFragment {
         ArrayAdapter<String > adapter = new ArrayAdapter<>(
                 inflater.getContext(),
                 android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pasta));
+                getResources().getStringArray(R.array.stores));
 
         return super.onCreateView(inflater, container, savedInstanceState);
     }
