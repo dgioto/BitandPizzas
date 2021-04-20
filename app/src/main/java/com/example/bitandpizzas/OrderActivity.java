1574669b9a33df42ecaa3626688db765fede1fab
Alexander Tih <dgioto@gmail.com>
2021-04-20 23:15:15 +0300

    добавление FAB-кнопки в OrderActivity

--- app/src/main/java/com/example/bitandpizzas/OrderActivity.java
+++ app/src/main/java/com/example/bitandpizzas/OrderActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -19,5 +20,9 @@ public class OrderActivity extends AppCompatActivity {
         //Добавление кнопки "Вверх"
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view){
+
     }
 }
